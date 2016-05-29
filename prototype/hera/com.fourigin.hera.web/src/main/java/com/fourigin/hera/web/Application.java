a2a663572b6d70cda0b94f70712fbcb8afd5080f
Vlad Satanovski <vlad.s@gmx.de>
2016-05-29 20:55:20 +0200

    Dynamic context-path support.

--- prototype/hera/com.fourigin.hera.web/src/main/java/com/fourigin/hera/web/Application.java
+++ prototype/hera/com.fourigin.hera.web/src/main/java/com/fourigin/hera/web/Application.java
@@ -1,10 +1,15 @@
 package com.fourigin.hera.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+
+import java.util.Properties;
 
 @Configuration
 @EnableAutoConfiguration
@@ -12,9 +17,24 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 public class Application {
 
+    @Value("${hera.context-path}")
+    private String contextPath;
+
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
         app.run(args);
     }
 
+    @Bean
+    public SimpleUrlHandlerMapping dynamicHeraServletMapping() {
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setOrder(Integer.MAX_VALUE - 2);
+
+        Properties urlProperties = new Properties();
+        urlProperties.put(contextPath + "/**", "heraController");
+
+        mapping.setMappings(urlProperties);
+
+        return mapping;
+    }
 }
