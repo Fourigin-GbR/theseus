bd1d34a1346da7cd536d545e725fb05b81f6316f
Vlad Satanovski <vlad.s@gmx.de>
2016-06-19 01:52:40 +0200

    ContentPageMetaData.Builder added.

--- prototype/cms/com.fourigin.cms.models/src/main/java/com/fourigin/cms/models/content/ContentPageMetaData.java
+++ prototype/cms/com.fourigin.cms.models/src/main/java/com/fourigin/cms/models/content/ContentPageMetaData.java
@@ -1,5 +1,6 @@
 package com.fourigin.cms.models.content;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ContentPageMetaData {
@@ -28,5 +29,46 @@ public class ContentPageMetaData {
           "title='" + title + '\'' +
           ", attributes=" + attributes +
           '}';
+    }
+
+    public static class Builder {
+        private String title;
+        private Map<String, String> attributes;
+
+        public Builder title(String title){
+            this.title = title;
+            return this;
+        }
+
+        public Builder attributes(Map<String, String> attributes){
+            this.attributes = attributes;
+            return this;
+        }
+
+        public Builder attribute(String name, String value){
+            if(name == null){
+                throw new IllegalArgumentException("Attribute name must not be null!");
+            }
+
+            if(attributes == null){
+                attributes = new HashMap<>();
+            }
+
+            if(value == null){
+                attributes.remove(name);
+            }
+            else {
+                attributes.put(name, value);
+            }
+
+            return this;
+        }
+
+        public ContentPageMetaData build(){
+            ContentPageMetaData metaData = new ContentPageMetaData();
+            metaData.setTitle(title);
+            metaData.setAttributes(attributes);
+            return metaData;
+        }
     }
 }
