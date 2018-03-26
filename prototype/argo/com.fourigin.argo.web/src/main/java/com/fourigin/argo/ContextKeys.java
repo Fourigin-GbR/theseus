721f688a66dfa389189e15ab5ce5cff814b8944a
Vlad Satanovski <vlad.s@gmx.de>
2018-03-26 11:00:06 +0200

    Added data_hotspots to ViewController

--- prototype/argo/com.fourigin.argo.web/src/main/java/com/fourigin/argo/ContextKeys.java
+++ prototype/argo/com.fourigin.argo.web/src/main/java/com/fourigin/argo/ContextKeys.java
@@ -4,4 +4,5 @@ public interface ContextKeys {
     String CONTENT_PAGE = "data_content";
     String PAGE_INFO = "data_page";
     String SITE_ATTRIBUTES = "data_attributes";
+    String HOTSPOTS = "data_hotspots";
 }
--- prototype/argo/com.fourigin.argo.web/src/main/java/com/fourigin/argo/compile/ViewController.java
+++ prototype/argo/com.fourigin.argo.web/src/main/java/com/fourigin/argo/compile/ViewController.java
@@ -2,9 +2,14 @@ package com.fourigin.argo.compile;
 
 import com.fourigin.argo.ContextKeys;
 import com.fourigin.argo.models.content.ContentPage;
+import com.fourigin.argo.models.content.ContentPagePrototype;
+import com.fourigin.argo.models.content.hotspots.ElementsEditorProperties;
 import com.fourigin.argo.models.structure.nodes.PageInfo;
+import com.fourigin.argo.models.template.Template;
+import com.fourigin.argo.models.template.TemplateReference;
 import com.fourigin.argo.repository.ContentRepositoryFactory;
 import com.fourigin.argo.repository.ContentResolver;
+import com.fourigin.argo.repository.TemplateResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
@@ -21,6 +27,8 @@ import java.util.Map;
 public class ViewController {
 
     private ContentRepositoryFactory contentRepositoryFactory;
+
+    private TemplateResolver templateResolver;
 
     private final Logger logger = LoggerFactory.getLogger(ViewController.class);
 
@@ -39,6 +47,30 @@ public class ViewController {
 
         PageInfo pageInfo = contentResolver.resolveInfo(PageInfo.class, path);
 
+        TemplateReference templateReference = pageInfo.getTemplateReference();
+        if(templateReference == null){
+            throw new IllegalStateException("No TemplateReference defined for PageInfo " + pageInfo);
+        }
+        if (logger.isDebugEnabled()) logger.debug("Template reference: {}", templateReference);
+
+        String templateId = templateReference.getTemplateId();
+        Template template = templateResolver.retrieve(templateId);
+        if(template == null){
+            throw new IllegalStateException("No template found for id '" + templateId + "'!");
+        }
+        if (logger.isDebugEnabled()) logger.debug("Template: {}", templateId);
+
+        Map<String, ElementsEditorProperties> hotspots;
+        ContentPagePrototype prototype = template.getPrototype();
+        if(prototype == null){
+            if (logger.isDebugEnabled()) logger.debug("No content prototype available for template '{}'", templateId);
+            hotspots = Collections.emptyMap();
+        }
+        else {
+            if (logger.isDebugEnabled()) logger.debug("Found content prototype for template '{}'", templateId);
+            hotspots = prototype.getHotspots();
+        }
+
         Map<String, String> siteAttributes = contentResolver.resolveSiteAttributes();
 
         ContentPage contentPage = contentResolver.retrieve(pageInfo);
@@ -53,6 +85,7 @@ public class ViewController {
         modelAndView.addObject(ContextKeys.CONTENT_PAGE, contentPage);
         modelAndView.addObject(ContextKeys.PAGE_INFO, pageInfo);
         modelAndView.addObject(ContextKeys.SITE_ATTRIBUTES, siteAttributes);
+        modelAndView.addObject(ContextKeys.HOTSPOTS, hotspots);
 
         return modelAndView;
     }
@@ -60,5 +93,10 @@ public class ViewController {
     @Autowired
     public void setContentRepositoryFactory(ContentRepositoryFactory contentRepositoryFactory) {
         this.contentRepositoryFactory = contentRepositoryFactory;
+    }
+
+    @Autowired
+    public void setTemplateResolver(TemplateResolver templateResolver) {
+        this.templateResolver = templateResolver;
     }
 }
