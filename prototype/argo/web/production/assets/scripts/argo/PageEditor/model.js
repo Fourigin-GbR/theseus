2d46ca82e8d1ce27b5f558771375c9f00493872b
Karsten Schaefer <karsten@karstenschaefer.de>
2018-05-10 15:31:32 +0200

    argo content elements json are being generated from editor form structure. Implement simple test-file.

--- prototype/argo/web/production/assets/scripts/argo/PageEditor/model.js
+++ prototype/argo/web/production/assets/scripts/argo/PageEditor/model.js
@@ -1,14 +1,12 @@
-if ("undefined" === typeof com)
-{
+if ("undefined" === typeof com) {
     var com;
 }
 com = com || {};
 com.fourigin = com.fourigin || {};
 com.fourigin.argo = com.fourigin.argo || {};
 com.fourigin.argo.pageEditor = com.fourigin.argo.pageEditor || {};
-com.fourigin.argo.pageEditor.Model = com.fourigin.argo.pageEditor.Model || (function ()
-{
-    var Model = function() {
+com.fourigin.argo.pageEditor.Model = com.fourigin.argo.pageEditor.Model || (function () {
+    var Model = function () {
         this.oContent = null;
         this.oHotspots = null;
         this.sPath = null;
@@ -21,7 +19,7 @@ com.fourigin.argo.pageEditor.Model = com.fourigin.argo.pageEditor.Model || (func
     Model.prototype.setBase = function (oBase) {
         this.oBase = oBase;
     };
-    Model.prototype.getBase = function() {
+    Model.prototype.getBase = function () {
         return this.oBase;
     };
 
@@ -29,7 +27,7 @@ com.fourigin.argo.pageEditor.Model = com.fourigin.argo.pageEditor.Model || (func
     Model.prototype.setPath = function (sPath) {
         this.sPath = sPath;
     };
-    Model.prototype.getPath = function() {
+    Model.prototype.getPath = function () {
         return this.sPath;
     };
 
@@ -38,18 +36,18 @@ com.fourigin.argo.pageEditor.Model = com.fourigin.argo.pageEditor.Model || (func
         this.oContent = oContent;
     };
     //
-    Model.prototype.getContent = function() {
+    Model.prototype.getContent = function () {
         return this.oContent;
     };
-    Model.prototype.getContentByPath = function(sPath) {
+    Model.prototype.getContentByPath = function (sPath) {
         // TODO: Supports only one-level search
         var sCleanPath = sPath.replace(/^\/|\/$/g, ''),
             aPath = sCleanPath.split("/"),
             self = this;
         //
-        for(var i=0, il=self.oContent.length; i<il; i++) {
-            if(self.oContent[i].name === aPath[0]) {
-               return self.oContent[i];
+        for (var i = 0, il = self.oContent.length; i < il; i++) {
+            if (self.oContent[i].name === aPath[0]) {
+                return self.oContent[i];
             }
         }
 
@@ -60,10 +58,119 @@ com.fourigin.argo.pageEditor.Model = com.fourigin.argo.pageEditor.Model || (func
     Model.prototype.setHotspots = function (oHotspots) {
         this.oHotspots = oHotspots;
     };
-    Model.prototype.getHotspots = function() {
+    Model.prototype.getHotspots = function () {
         return this.oHotspots;
     };
 
+
+    Model.prototype.generateArgoContentElementJsonByMarkup = function (jMarkupElement, oGeneratedArgoContent) {
+        oGeneratedArgoContent = oGeneratedArgoContent || {};
+        if (1 === jMarkupElement.length && jMarkupElement.is("fieldset")) {
+            var sType = jMarkupElement.attr("data-type");
+            switch (sType) {
+                case "text":
+                    oGeneratedArgoContent = this.generateArgoContentElementJsonTypeText(jMarkupElement);
+                    break;
+                case "object":
+                    oGeneratedArgoContent = this.generateArgoContentElementJsonTypeObject(jMarkupElement);
+                    break;
+                case "list":
+                    oGeneratedArgoContent = this.generateArgoContentElementJsonTypeList(jMarkupElement);
+                    break;
+                case "listGroup":
+                    oGeneratedArgoContent = this.generateArgoContentElementJsonTypeListGroup(jMarkupElement);
+                    break;
+                default:
+                    oGeneratedArgoContent.error = "Type is not supported ('" + sType + "')";
+                    break;
+            }
+        }
+        return oGeneratedArgoContent;
+    };
+    Model.prototype.generateArgoContentElementJsonTypeText = function (jMarkupElement) {
+        var oText = {
+                "type": "text",
+                "name": null,
+                "content": null
+            },
+            sName = jMarkupElement.find("input[name='name']").val(),
+            sContent = jMarkupElement.find("textarea[name='content']").text();
+        //
+        if (sName) {
+            oText.name = sName;
+        }
+        if (sContent) {
+            oText.content = sContent;
+        }
+        //
+        return oText;
+    };
+    Model.prototype.generateArgoContentElementJsonTypeObject = function (jMarkupElement) {
+        var oObject = {
+                "type": "object",
+                "name": null,
+                "referenceId": null,
+                "source": null,
+                "alternateText": null,
+                "mimeType": null
+            },
+            sName = jMarkupElement.find("input[name='name']").val(),
+            sReferenceId = jMarkupElement.find("input[name='referenceId']").val(),
+            sSource = jMarkupElement.find("input[name='source']").val(),
+            sAlternativeText = jMarkupElement.find("input[name='alternativeText']").val(),
+            sMimeType = jMarkupElement.find("input[name='mimeType']").val();
+        //
+        if (sName) {
+            oObject.name = sName;
+        }
+        if (sReferenceId) {
+            oObject.referenceId = sReferenceId;
+        }
+        if (sSource) {
+            oObject.source = sSource;
+        }
+        if (sAlternativeText) {
+            oObject.alternateText = sAlternativeText;
+        }
+        if (sMimeType) {
+            oObject.mimeType = sMimeType;
+        }
+        //
+        return oObject;
+    };
+    Model.prototype.generateArgoContentElementJsonTypeList = function (jMarkupElement) {
+        var oList = {
+                "type": "list",
+                "name": null,
+                "elements": []
+            },
+            sName = jMarkupElement.find("input[name='name']").val(),
+            jElements = jMarkupElement.find(" > fieldset > fieldset"),
+            self = this;
+        //
+        if (sName) {
+            oList.name = sName;
+        }
+        jElements.each(function(){
+            oList.elements.push(self.generateArgoContentElementJsonByMarkup(jQuery(this)));
+        });
+        //
+        return oList;
+    };
+    Model.prototype.generateArgoContentElementJsonTypeListGroup = function (jMarkupElement) {
+        var oListGroup = {
+                "type": "list-group",
+                "elements": []
+            },
+            jElements = jMarkupElement.find(" > fieldset > fieldset"),
+            self = this;
+        //
+        jElements.each(function(){
+            oListGroup.elements.push(self.generateArgoContentElementJsonByMarkup(jQuery(this)));
+        });
+        //
+        return oListGroup;
+    };
     //
     return Model;
 }());
