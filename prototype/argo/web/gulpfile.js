a657d3705b7b76ab8a057da9b3b992cd818cbca6
Karsten Schaefer <karsten@karstenschaefer.de>
2018-08-07 11:48:21 +0200

    Overlay: Header hinzugefügt. Header styled. Gulp: Watch hinzugefügt.

--- prototype/argo/web/gulpfile.js
+++ prototype/argo/web/gulpfile.js
@@ -5,10 +5,12 @@ var gulp = require("gulp"),
     sass = require("gulp-sass");
 //
 //
-gulp.task("default", function () {
+gulp.task("sass", function () {
     return gulp.src("resources/scss/**/*.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(gulp.dest("production/assets/styles"));
 });
 
-
+gulp.task("sass:watch", function () {
+    gulp.watch("resources/scss/**/*.scss", ["sass"]);
+});
