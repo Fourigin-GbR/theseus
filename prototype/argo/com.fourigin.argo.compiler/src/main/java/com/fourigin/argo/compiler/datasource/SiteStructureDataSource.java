529754ab1d376a27ddc8cb49614afd1be40152a6
Vlad Satanovski <vlad.s@gmx.de>
2018-09-14 20:23:15 +0200

    Including referenced content

--- prototype/argo/com.fourigin.argo.compiler/src/main/java/com/fourigin/argo/compiler/datasource/SiteStructureDataSource.java
+++ prototype/argo/com.fourigin.argo.compiler/src/main/java/com/fourigin/argo/compiler/datasource/SiteStructureDataSource.java
@@ -1,5 +1,8 @@
 package com.fourigin.argo.compiler.datasource;
 
+import com.fourigin.argo.models.content.ContentPage;
+import com.fourigin.argo.models.content.ContentPageManager;
+import com.fourigin.argo.models.content.UnresolvableContentPathException;
 import com.fourigin.argo.models.content.elements.ContentElement;
 import com.fourigin.argo.models.content.elements.LinkElement;
 import com.fourigin.argo.models.content.elements.TextContentElement;
@@ -10,9 +13,13 @@ import com.fourigin.argo.models.datasource.index.FieldDefinition;
 import com.fourigin.argo.models.datasource.index.FieldValue;
 import com.fourigin.argo.models.datasource.index.IndexAwareDataSource;
 import com.fourigin.argo.models.datasource.index.IndexDefinition;
+import com.fourigin.argo.models.structure.CompileState;
 import com.fourigin.argo.models.structure.PageState;
 import com.fourigin.argo.models.structure.nodes.PageInfo;
+import com.fourigin.argo.models.template.TemplateReference;
 import com.fourigin.argo.repository.ContentResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -25,9 +32,11 @@ import java.util.Set;
 public class SiteStructureDataSource implements
     DataSource<SiteStructureDataSourceQuery>,
     IndexAwareDataSource<SiteStructureDataSourceQuery>,
-    ContentResolverAwareDataSource
-{
+    ContentResolverAwareDataSource {
+
     public static final String TYPE = "SITE";
+
+    private final Logger logger = LoggerFactory.getLogger(SiteStructureDataSource.class);
 
     @Override
     public String getType() {
@@ -50,8 +59,8 @@ public class SiteStructureDataSource implements
         Collection<PageInfo> infos = contentResolver.resolveInfos(path);
         if (infos != null && !infos.isEmpty()) {
             for (PageInfo info : infos) {
-                if(query.isIgnoreOwnerPage()) {
-                    if(info.equals(ownerPage)){
+                if (query.isIgnoreOwnerPage()) {
+                    if (info.equals(ownerPage)) {
                         continue;
                     }
                 }
@@ -64,18 +73,69 @@ public class SiteStructureDataSource implements
                     .withContent(info.getDisplayName());
 
                 if (query.isVerbose()) {
+                    CompileState compileState = state.getCompileState();
+                    TemplateReference templateReference = info.getTemplateReference();
+                    PageInfo.ContentPageReference contentReference = info.getContentPageReference();
+
                     textBuilder
-                        .withAttribute("description", info.getDescription())
-                        .withAttribute("localizedName", info.getLocalizedName())
-                        .withAttribute("compileState", String.valueOf(state.getCompileState()))
-                        .withAttribute("contentPageReference", String.valueOf(info.getContentPageReference()))
-                        .withAttribute("templateReference", String.valueOf(info.getTemplateReference()));
+                        .withAttribute("info.description", info.getDescription())
+                        .withAttribute("info.localizedName", info.getLocalizedName())
+                        .withAttribute("info.state.staged", String.valueOf(state.isStaged()))
+                        .withAttribute("info.state.checksum", String.valueOf(state.getChecksum()))
+                        .withAttribute("info.state.revision", state.getRevision());
+
+                    if(templateReference != null) {
+                        textBuilder
+                            .withAttribute("info.template.templateId", templateReference.getTemplateId())
+                            .withAttribute("info.template.variationId", templateReference.getVariationId())
+                            .withAttribute("info.template.revision", templateReference.getRevision());
+                    }
+
+                    if(contentReference != null) {
+                        textBuilder
+                            .withAttribute("info.content.parent", contentReference.getParentPath())
+                            .withAttribute("info.content.id", contentReference.getContentId());
+                    }
+
+                    if(compileState != null) {
+                        textBuilder
+                            .withAttribute("info.state.compileState.checksum", compileState.getChecksum())
+                            .withAttribute("info.state.compileState.compiled", String.valueOf(compileState.isCompiled()))
+                            .withAttribute("info.state.compileState.timestamp", String.valueOf(compileState.getTimestamp()))
+                            .withAttribute("info.state.compileState.message", compileState.getMessage());
+                    }
                 }
 
                 LinkElement.Builder linkBuilder = new LinkElement.Builder()
                     .withTarget(info.getReference())
                     .withName(info.getName())
                     .withElement(textBuilder.build());
+
+                List<String> contentReferences = query.getContentReferences();
+                if (contentReferences != null && !contentReferences.isEmpty()) {
+                    ContentPage contentPage = contentResolver.retrieve(info);
+
+                    for (String reference : contentReferences) {
+                        ContentElement resolved;
+                        try {
+                            resolved = ContentPageManager.resolve(contentPage, reference);
+                            if (resolved == null) {
+                                if (logger.isWarnEnabled())
+                                    logger.warn("Unable to resolve content reference {} for {}", reference, info);
+                                continue;
+                            }
+                        }
+                        catch(UnresolvableContentPathException ex){
+                            if (logger.isWarnEnabled())
+                                logger.warn("Unable to resolve content reference {} for {}", reference, info);
+                            continue;
+                        }
+
+                        if (logger.isInfoEnabled())
+                            logger.info("Adding resolved content element for reference {} from {}", reference, info);
+                        linkBuilder.withElement(resolved);
+                    }
+                }
 
                 result.add(linkBuilder.build());
             }
@@ -96,7 +156,7 @@ public class SiteStructureDataSource implements
         DataSourceIndex index = new DataSourceIndex();
 
         IndexDefinition indexDefinition = id.getIndex();
-        if(indexDefinition == null) {
+        if (indexDefinition == null) {
             return null;
         }
 
@@ -128,7 +188,7 @@ public class SiteStructureDataSource implements
         index.setCategories(categories);
         index.setFields(fields);
         index.setSearchValues(searchValues);
-        
+
         return index;
     }
 }
--- prototype/argo/com.fourigin.argo.compiler/src/main/java/com/fourigin/argo/compiler/datasource/SiteStructureDataSourceQuery.java
+++ prototype/argo/com.fourigin.argo.compiler/src/main/java/com/fourigin/argo/compiler/datasource/SiteStructureDataSourceQuery.java
@@ -3,19 +3,25 @@ package com.fourigin.argo.compiler.datasource;
 import com.fourigin.argo.models.datasource.DataSourceQuery;
 import com.fourigin.argo.models.datasource.DataSourceQueryCreationException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class SiteStructureDataSourceQuery implements DataSourceQuery {
 
     private static final String INFO_PATH = "INFO_PATH";
     private static final String VERBOSE = "VERBOSE";
     private static final String IGNORE_OWNER_PAGE = "IGNORE_OWNER_PAGE";
+    private static final String INCLUDE_CONTENT = "INCLUDE_CONTENT";
 
     private String path;
 
     private boolean verbose = true;
 
     private boolean ignoreOwnerPage = false;
+
+    private List<String> contentReferences;
 
     @Override
     public void buildFromMap(Map<String, Object> values) {
@@ -33,6 +39,16 @@ public class SiteStructureDataSourceQuery implements DataSourceQuery {
         this.verbose = Boolean.parseBoolean(String.valueOf(values.get(VERBOSE)));
 
         this.ignoreOwnerPage = Boolean.parseBoolean(String.valueOf(values.get(IGNORE_OWNER_PAGE)));
+
+        Object contentToInclude = values.get(INCLUDE_CONTENT);
+        if (contentToInclude != null) {
+            contentReferences = new ArrayList<>();
+
+            String[] parts = String.valueOf(contentToInclude).split(",");
+            for (String part : parts) {
+                contentReferences.add(part.trim());
+            }
+        }
     }
 
     public String getPath() {
@@ -45,5 +61,35 @@ public class SiteStructureDataSourceQuery implements DataSourceQuery {
 
     public boolean isIgnoreOwnerPage() {
         return ignoreOwnerPage;
+    }
+
+    public List<String> getContentReferences() {
+        return contentReferences;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SiteStructureDataSourceQuery)) return false;
+        SiteStructureDataSourceQuery that = (SiteStructureDataSourceQuery) o;
+        return verbose == that.verbose &&
+            ignoreOwnerPage == that.ignoreOwnerPage &&
+            Objects.equals(path, that.path) &&
+            Objects.equals(contentReferences, that.contentReferences);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(path, verbose, ignoreOwnerPage, contentReferences);
+    }
+
+    @Override
+    public String toString() {
+        return "SiteStructureDataSourceQuery{" +
+            "path='" + path + '\'' +
+            ", verbose=" + verbose +
+            ", ignoreOwnerPage=" + ignoreOwnerPage +
+            ", contentReferences=" + contentReferences +
+            '}';
     }
 }
