95ad97bef8ea219892fb46d05b5fe43f45cb4f3e
Karsten Schaefer <karsten@karstenschaefer.de>
2018-09-25 22:59:27 +0200

    Add functions to handle child-fieldsets, dependend on checkboxes, who toggle their status.

---
+++ prototype/argo/cms-root/templates/tsp/assets/scripts/Forms.js
@@ -0,0 +1,24 @@
+var inputs = document.getElementsByTagName("input");
+var fieldsets = document.getElementsByTagName("fieldset");
+console.log("inputs, fieldsets", inputs, fieldsets);
+
+var inputsWithBoundFieldSets = Array.prototype.filter.call(inputs, function(element, index, aElements) {
+    return element.hasAttributes("data-activate-fieldset-name");
+});
+
+var _inputsWithBoundFieldSets = null; //inputs.querySelectorAll("[data-activate-fieldset-name]");
+
+Array.prototype.forEach.call(inputsWithBoundFieldSets, function(el){
+    var targetFieldset = Array.prototype.filter.call(fieldsets, function(element, index, aElements) {
+        return (element.getAttribute("name") === el.getAttribute('data-activate-fieldset-name'));
+    })[0];
+    el.addEventListener("change", function() {
+        console.info("Changed. Target is: ", targetFieldset);
+        if(!this.checked) {
+            targetFieldset.setAttribute("disabled", "disabled")
+        }
+        else {
+            targetFieldset.removeAttribute("disabled");
+        }
+    });
+});
