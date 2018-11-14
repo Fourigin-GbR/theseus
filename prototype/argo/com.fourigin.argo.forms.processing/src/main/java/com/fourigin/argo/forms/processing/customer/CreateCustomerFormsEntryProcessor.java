b4a0a083a552c5066bdbda237b5ff6d2b42c7642
Vlad Satanovski <vlad.s@gmx.de>
2018-11-14 22:39:30 +0100

    customer.id generation

--- prototype/argo/com.fourigin.argo.forms.processing/src/main/java/com/fourigin/argo/forms/processing/customer/CreateCustomerFormsEntryProcessor.java
+++ prototype/argo/com.fourigin.argo.forms.processing/src/main/java/com/fourigin/argo/forms/processing/customer/CreateCustomerFormsEntryProcessor.java
@@ -5,7 +5,12 @@ import com.fourigin.argo.forms.FormsEntryProcessor;
 import com.fourigin.argo.forms.FormsRegistry;
 import com.fourigin.argo.forms.FormsStoreRepository;
 import com.fourigin.argo.forms.customer.Customer;
+import com.fourigin.argo.forms.models.FormsStoreEntryInfo;
 import com.fourigin.argo.forms.models.ProcessingHistoryRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class CreateCustomerFormsEntryProcessor implements FormsEntryProcessor {
     public static final String NAME = "CREATE-CUSTOMER";
@@ -14,6 +19,8 @@ public class CreateCustomerFormsEntryProcessor implements FormsEntryProcessor {
 
     private FormsStoreRepository formsStoreRepository;
     private CustomerRepository customerRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(CreateCustomerFormsEntryProcessor.class);
 
     public CreateCustomerFormsEntryProcessor(
         FormsStoreRepository formsStoreRepository,
@@ -31,6 +38,24 @@ public class CreateCustomerFormsEntryProcessor implements FormsEntryProcessor {
     @Override
     public ProcessingHistoryRecord processEntry(String entryId, FormsRegistry registry) {
         Customer customer = formsStoreRepository.getObjectAttachment(entryId, ATTACHMENT_NAME, Customer.class);
+
+        // search for the next free id
+        int newId = 1;
+        String newIdValue = String.valueOf(newId);
+
+        List<String> availableIds = customerRepository.listCustomerIds();
+        while(availableIds.contains(newIdValue)){
+            newIdValue = String.valueOf(newId++);
+        }
+
+        if (logger.isDebugEnabled()) logger.debug("Generated customer id: '{}'", newIdValue);
+
+        customer.setId(newIdValue);
+
+        FormsStoreEntryInfo info = formsStoreRepository.retrieveEntryInfo(entryId);
+        info.getHeader().setCustomer(newIdValue);
+        formsStoreRepository.updateEntryInfo(info);
+
         customerRepository.createCustomer(customer);
 
         ProcessingHistoryRecord record = new ProcessingHistoryRecord();
