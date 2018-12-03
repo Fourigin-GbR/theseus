2292c7da02e82f23ba008897067ebfc229dc4a87
Vlad Satanovski <vlad.s@gmx.de>
2018-12-03 17:30:11 +0100

    stage & dev environments

--- prototype/argo/com.fourigin.argo.forms.dashboard/sync.sh
+++ prototype/argo/com.fourigin.argo.forms.dashboard/sync.sh
@@ -4,7 +4,21 @@ APPName="argo-forms-dashboard"
 APPVersion="1.0-SNAPSHOT"
 JARFile="$APPName-$APPVersion.jar"
 SERVER="fourigin.de"
-TARGETPath="$APPRoot/forms-dashboard"
+
+mode=$1
+case "$mode" in
+dev)
+    echo "Synchronizing with DEV environment"
+    TARGETPath="$APPRoot/dev-forms-dashboard"
+    ;;
+stage)
+    echo "Synchronizing with STAGE environment"
+    TARGETPath="$APPRoot/forms-dashboard"
+    ;;
+*)
+    echo "Use 'dev' or 'stage' as a command line parameter, to define the sync target"
+    exit 0;
+esac
 
 rsync -avz build/libs/$JARFile $SERVER:$TARGETPath
 
--- prototype/argo/com.fourigin.argo.forms.web/sync.sh
+++ prototype/argo/com.fourigin.argo.forms.web/sync.sh
@@ -4,7 +4,21 @@ APPName="argo-forms"
 APPVersion="1.0-SNAPSHOT"
 JARFile="$APPName-$APPVersion.jar"
 SERVER="fourigin.de"
-TARGETPath="$APPRoot/forms"
+
+mode=$1
+case "$mode" in
+dev)
+    echo "Synchronizing with DEV environment"
+    TARGETPath="$APPRoot/dev-forms"
+    ;;
+stage)
+    echo "Synchronizing with STAGE environment"
+    TARGETPath="$APPRoot/forms"
+    ;;
+*)
+    echo "Use 'dev' or 'stage' as a command line parameter, to define the sync target"
+    exit 0;
+esac
 
 rsync -avz build/libs/$JARFile $SERVER:$TARGETPath
 
