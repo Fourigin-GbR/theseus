4c7028ff7f1a09fc8c59a0972599a40ce859400a
Karsten Schaefer <karsten@karstenschaefer.de>
2018-12-22 23:10:34 +0100

    Static prototype for greece version 1: Search form basics are working.

---
+++ websites-prototypes/greece/version-1/assets/scripts/search-result.js
@@ -0,0 +1,86 @@
+var searchResult = function() {
+    this.jForm = jQuery("form#searchObjects");
+    this.jResultItemPrototype = null;
+    this.jResultItemImagePrototype = null;
+
+    searchResult.prototype.fetchPrototypes = function() {
+        this.jResultItemImagePrototype = jQuery("[data-prototype='search-result-item-image']").remove().clone().removeAttr("data-prototype");
+        this.jResultItemPrototype = jQuery("[data-prototype='search-result-item']").remove().clone().removeAttr("data-prototype");
+    };
+
+    searchResult.prototype.showResults = function(data) {
+        var results = data || this.results;
+        var jTarget = jQuery("[data-element='search-result-list-container']");
+
+        jTarget.empty();
+
+        for (var key in results) {
+            if(results.hasOwnProperty(key)) {
+                var jPrototype = this.jResultItemPrototype.clone();
+                // Data:
+                jPrototype.find("[data-content='category']").html(results[key]["category"]);
+                jPrototype.find("[data-content='description']").html(results[key]["description"]);
+                // Images:
+                if(results[key]["images"]) {
+                    var jImagesTarget = jPrototype.find("[data-element='search-result-item-images-container']");
+                    for(var i=0, il= results[key]["images"].length; i<il; i++) {
+                        var imagePrototype = this.jResultItemImagePrototype.clone();
+                        imagePrototype.find("[data-content='image-src']").attr("src", results[key]["images"][i]);
+                        jImagesTarget.append(imagePrototype);
+                    }
+                }
+                // Append to DOM:
+                jTarget.append(jPrototype);
+                this.applySlider(jTarget);
+            }
+        }
+    };
+
+    searchResult.prototype.setEvents = function() {
+        var self = this;
+
+        this.jForm.find("input, select").on("change", function() {
+            self.jForm.trigger("submit");
+        });
+
+        this.jForm.on("submit", function(e) {
+            e.preventDefault();
+            self.loadResultList();
+        });
+    };
+
+    searchResult.prototype.loadResultList = function() {
+        var self = this;
+        jQuery.ajax({
+            dataType: "json",
+            url: "immobilien.json",
+            data: this.jForm.serialize(),
+            success: function(data){self.showResults(data)}
+        });
+    };
+
+    searchResult.prototype.applySlider = function(jTarget) {
+        jTarget.find(".slider").each(function () {
+            new Slider(
+                jQuery(this),
+                {
+                    "slideItemsAmountPerPage": [
+                        {
+                            "min": "0px",
+                            "max": null,
+                            "slideItems": 1
+                        }
+                    ]
+                }
+            );
+        });
+    };
+
+    this.fetchPrototypes();
+    this.setEvents();
+};
+
+jQuery("document").ready(function() {
+    var sr = new searchResult();
+    sr.loadResultList();
+});
---
+++ websites-prototypes/greece/version-1/assets/scripts/slider.js
@@ -0,0 +1,197 @@
+function Slider(slider, configuration) {
+    var self = this;
+    //
+    this.sliderPageStorage = slider.find("input.sliderPageStorage");
+    this.slideHandler = slider.find(".sliderWrapper").find("ul");
+    this.slideItems = this.slideHandler.find("li");
+    this.sliderSlideLeft = slider.find(".sliderSlideLeft");
+    this.sliderSlideRight = slider.find(".sliderSlideRight");
+    this.pagination = slider.find(".sliderPagination");
+    this.slidesPerPage = null;
+    this.pagesAmount = 1;
+    this.currentLeftPosition = null;
+    this.matchMedias = [];
+    this.configuration = configuration;
+    //
+    // Default: Start on page 1
+    if(!this.getCurrentPage()) {
+        this.setCurrentPage(1);
+    }
+    //
+    //
+    // Initial:
+    this.getAndApplyAmountOfSlides();
+    this.generatePagination();
+    this.slideToPage(this.getCurrentPage());
+    this.setCurrentPageActiveInPagination();
+    this.updateVisibleStatusOfSlideButtons();
+    //
+    this.sliderSlideLeft.on("click", function (e) {
+        e.stopPropagation();
+        self.slideByDirection("left")
+    });
+    this.sliderSlideRight.on("click", function (e) {
+        self.slideByDirection("right")
+    });
+}
+//
+Slider.prototype.getCurrentPage = function () {
+    return parseInt(this.sliderPageStorage.val());
+};
+Slider.prototype.setCurrentPage = function (page) {
+    return this.sliderPageStorage.val(parseInt(page));
+};
+//
+Slider.prototype.generatePagination = function() {
+    this.pagination.find("li").remove();
+    for(var i=0, il=this.pagesAmount; i !== il; i++) {
+        this.pagination.find("ul").append("<li></li>");
+    }
+};
+//
+Slider.prototype.setCurrentPageActiveInPagination = function() {
+    var classNameActive = "active",
+        currentPageIndex = this.getCurrentPage() - 1,
+        paginationItems = this.pagination.find("li");
+    //
+    // reset:
+    paginationItems.removeClass(classNameActive);
+    paginationItems.eq(currentPageIndex).addClass(classNameActive);
+};
+//
+//
+Slider.prototype.getCurrentPageBySliderPosition = function() {
+    return (parseInt(this.slideHandler[0].style.left) / 100 * (-1)) + 1;
+};
+//
+Slider.prototype.getLeftPositionByPage = function(page) {
+    return (page - 1) * 100 * (-1);
+};
+//
+//
+Slider.prototype.slideToPage = function(page) {
+    var newLeftPosition = this.getLeftPositionByPage(page);
+    //
+    this.slideHandler[0].style.left = newLeftPosition + "%";
+    this.setCurrentPage(page);
+    //
+    this.updateVisibleStatusOfSlideButtons();
+    this.setCurrentPageActiveInPagination();
+};
+//
+Slider.prototype.getAndApplyAmountOfSlides = function () {
+    var self = this;
+    //
+    for(var i=0, il= this.configuration.slideItemsAmountPerPage.length; i!==il; i++) {
+
+        (function() {
+            var slideAmount = self.configuration.slideItemsAmountPerPage[i],
+                minWidth = slideAmount.min ? slideAmount.min : null,
+                maxWidth = slideAmount.max ? slideAmount.max : null,
+                queries = [],
+                matchMedia = {"match": null, "callback": null};
+            //
+            minWidth ? queries.push("(min-width: " + minWidth + ")") : "";
+            maxWidth ? queries.push("(max-width: " + maxWidth + ")") : "";
+            //
+            matchMedia.match = window.matchMedia(
+                queries.join(" and ")
+            );
+
+            var cb = function (matchMediaObject, resizeEvent) {
+                //
+                if(matchMediaObject.matches) {
+                    self.pagesAmount = Math.ceil(self.slideItems.length / slideAmount.slideItems);
+                    self.slideItems.css("width", 100 / slideAmount.slideItems + "%");
+                    //
+                    // On resize: Go back to page 1:
+                    if(resizeEvent) self.slideToPage(1);
+                    self.updateVisibleStatusOfSlideButtons();
+                    self.generatePagination();
+                    self.setCurrentPageActiveInPagination();
+                }
+            };
+
+            if (matchMedia.match.matches) {
+                cb(matchMedia.match);
+            }
+            matchMedia.match.addListener(function(matchMediaObject){
+                cb(matchMediaObject, true);
+            });
+            //
+            self.matchMedias.push(matchMedia);
+        })()
+    }
+};
+//
+//
+Slider.prototype.updateVisibleStatusOfSlideButtons = function() {
+    var hideButtonClassName = "inactive",
+        currentPage = this.getCurrentPage();
+    //
+    if(this.pagesAmount === 1) {
+        this.sliderSlideLeft.addClass(hideButtonClassName);
+        this.sliderSlideRight.addClass(hideButtonClassName);
+        return;
+    }
+    //
+    if(currentPage === 1) {
+        this.sliderSlideLeft.addClass(hideButtonClassName);
+    }
+    else {
+        this.sliderSlideLeft.removeClass(hideButtonClassName);
+    }
+
+    if(currentPage === this.pagesAmount) {
+        this.sliderSlideRight.addClass(hideButtonClassName);
+    }
+    else {
+        this.sliderSlideRight.removeClass(hideButtonClassName);
+    }
+};
+//
+Slider.prototype.showOrHideSlideButtons = function(action, buttons) {
+    var hideButtonClassName = "inactive";
+    //
+    for(var i=0, il=buttons.length; i !== il; i++) {
+        switch (buttons[i]) {
+            case "slideLeft":
+                action === "show" ? this.sliderSlideLeft.removeClass(hideButtonClassName) : this.sliderSlideLeft.addClass(hideButtonClassName);
+                break;
+            case "slideRight":
+                action === "show" ? this.sliderSlideRight.removeClass(hideButtonClassName) : this.sliderSlideRight.addClass(hideButtonClassName);
+                break;
+        }
+    }
+};
+//
+Slider.prototype.slideByDirection = function (direction) {
+    var newLeftPosition, currentPage, newPage;
+    //
+    currentPage = this.getCurrentPage();
+    //
+    switch (direction) {
+        case "left":
+            if (currentPage === 1) {
+                newLeftPosition = 0;
+            }
+            else {
+                newPage = currentPage - 1;
+                newLeftPosition = this.getLeftPositionByPage(newPage);
+            }
+            break;
+        case "right":
+            if (currentPage >= this.pagesAmount) {
+                newLeftPosition = this.getLeftPositionByPage(this.pagesAmount);
+            }
+            else {
+                newPage = currentPage + 1;
+                newLeftPosition = this.getLeftPositionByPage(newPage);
+            }
+            break;
+        default:
+            newLeftPosition = 0;
+            newPage = 1;
+    }
+    this.slideToPage(newPage);
+};
