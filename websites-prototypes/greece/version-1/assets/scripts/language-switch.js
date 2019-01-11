6104df3637a77a63eec8a390066bb3def3420037
Karsten Schaefer <karsten@karstenschaefer.de>
2019-01-11 13:50:48 +0100

    Implement: language-switch on select-change.

--- websites-prototypes/greece/version-1/assets/scripts/language-switch.js
+++ websites-prototypes/greece/version-1/assets/scripts/language-switch.js
@@ -5,6 +5,7 @@ jQuery("document").ready(function(){
         var selectedValue = jSelector.val(),
             newUrl = null;
         switch (selectedValue) {
+            // TODO: Remove that switch, only take the real URL values, after the handmade prototype-page past away.
             case "de":
                 newUrl = "http://www.de.bestgreekestate.com/";
                 break;
@@ -13,6 +14,9 @@ jQuery("document").ready(function(){
                 break;
             case "en":
                 newUrl = "http://www.en.bestgreekestate.com/";
+                break;
+            default:
+                newUrl = selectedValue;
                 break;
         }
         if(newUrl){
