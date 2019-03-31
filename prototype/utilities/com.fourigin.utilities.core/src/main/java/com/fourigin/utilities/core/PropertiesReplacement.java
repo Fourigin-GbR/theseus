8a16ea42dc05e09c20a76025ec774aa456f1b7b3
Vlad Satanovski <vlad.s@gmx.de>
2019-03-31 10:30:05 +0200

    Placeholder replacement bugfix

--- prototype/utilities/com.fourigin.utilities.core/src/main/java/com/fourigin/utilities/core/PropertiesReplacement.java
+++ prototype/utilities/com.fourigin.utilities.core/src/main/java/com/fourigin/utilities/core/PropertiesReplacement.java
@@ -1,5 +1,8 @@
 package com.fourigin.utilities.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -12,6 +15,8 @@ public class PropertiesReplacement {
     private boolean keepUnresolved = true;
 
     private static final String DEFAULT_REPLACEMENT_PATTERN = "\\$\\{(.+?)\\}";
+
+    private final Logger logger = LoggerFactory.getLogger(PropertiesReplacement.class);
 
     public PropertiesReplacement() {
         pattern = Pattern.compile(DEFAULT_REPLACEMENT_PATTERN);
@@ -30,15 +35,21 @@ public class PropertiesReplacement {
             throw new IllegalArgumentException("Invalid number of properties! Required an even number, but found " + properties.length + " !");
         }
 
+        if (logger.isDebugEnabled()) logger.debug("text: '{}', properties: {}", text, properties);
+
         Map<String, String> props = new HashMap<>();
-        for (int i = 0; i < properties.length / 2; i += 2) {
+        for (int i = 0; i < properties.length; i += 2) {
             props.put(properties[i], properties[i + 1]);
         }
 
+        if (logger.isDebugEnabled()) logger.debug("Properties converted to {}", props);
+        
         return process(text, props);
     }
 
     public String process(String text, Map<String, String> properties) {
+        if (logger.isDebugEnabled()) logger.debug("Processing text '{}' with placeholders {}", text, properties);
+
         Matcher matcher = pattern.matcher(text);
 
         StringBuilder builder = new StringBuilder();
@@ -46,7 +57,7 @@ public class PropertiesReplacement {
         while (matcher.find()) {
             String replacement = properties.get(matcher.group(1));
 
-            builder.append(text.substring(i, matcher.start()));
+            builder.append(text, i, matcher.start());
             if (replacement == null) {
                 if (keepUnresolved) {
                     builder.append(matcher.group(0));
@@ -57,9 +68,13 @@ public class PropertiesReplacement {
 
             i = matcher.end();
         }
-        builder.append(text.substring(i, text.length()));
+        builder.append(text, i, text.length());
 
-        return builder.toString();
+        String result = builder.toString();
+
+        if (logger.isDebugEnabled()) logger.debug("Processed to '{}'", result);
+
+        return result;
     }
 
     public void setKeepUnresolved(boolean keepUnresolved) {
